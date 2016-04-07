9d831e22d8e42da9501bd63b57242af59e26b671
Diaz Lujan <jesus.diaz.lujan@accenture.com>
2016-04-07 17:12:43 -0500

    Proyecto con Jenkins

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install -y oracle-java7-installer
+apt-get install oracle-java7-set-default
+
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb <http://pkg.jenkins-ci.org/debian binary/> > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install -y jenkins
