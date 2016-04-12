8bd5cd86e2849ba5be1119e9be2ef697974e2c23
Diaz Lujan <jesus.diaz.lujan@accenture.com>
2016-04-12 14:18:09 -0500

    prueba

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,3 +16,9 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add
 sh -c 'echo deb <http://pkg.jenkins-ci.org/debian binary/> > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install -y jenkins
+
+mkdir /git
+cd /git
+wget https://git-core.googlecode.com/files/git-1.8.1.2.tar.gz
+tar -zxf git-1.8.1.2.tar.gz
+apt-get install git
